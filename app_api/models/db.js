9950eb170e3e3cfdb7fe2ddffa52884c7b9a4157
Hakan Körhasan <hakankorhasann@gmail.com>
2022-11-20 19:56:49 +0300

    commit

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,9 +1,7 @@
 var mongoose = require("mongoose");
-//var dbUrl = 'mongodb://localhost/mekanbul';
-   // "mongodb+srv://hakankorhasan:1234@mekanbul.civlwxs.mongodb.net/mekanbul?retryWrites=true&w=majority";
-var dbUrl = "mongodb+srv://hakankorhasan:1234@mekanbul.femahp8.mongodb.net/?retryWrites=true&w=majority";
-
 require("./mekansema");
+var dbUrl = 'mongodb+srv://hakankorhasan:1234@mekanbul.femahp8.mongodb.net/?retryWrites=true&w=majority';
+
 mongoose.connect(dbUrl);
 
 mongoose.connection.on("connected", () => {
--- readme.md
+++ readme.md
@@ -1,8 +1,1 @@
 
-/api/mekanlar?enlem=37.7&boylam=30.5
-
- 2. [Mekan ID ile mekanın bilgilerini getir](https://mekanbul-2.hakankorhasann.repl.co/api/mekanlar/637507696a0b67843adfd837).
-![Mekan ID ile mekanın bilgilerini getir](./resimler/mekan_bilgileri.png)
-3. [Bir Mekana Ait Belirli Bir Yorumu Getir](https://mekanbul-2.hakankorhasann.repl.co/api/mekanlar/637507696a0b67843adfd837/yorumlar/6375077c1faab8a0f259a2bf). 
-![Bir Mekana Ait Belirli Bir Yorumu Getir](./resimler/mekan_yorum.png)
-
